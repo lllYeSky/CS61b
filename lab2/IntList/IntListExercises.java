6f26f056b842ca44e889162f660c063910963bc3
lllYeSky <2889377622@qq.com>
2026-07-20 13:25:00 +0800

    finish lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,11 +10,10 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
-        head.first += c;
     }
 
     /**
@@ -80,6 +79,7 @@ public class IntListExercises {
             lst.first *= lst.first;
         }
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        boolean sq = squarePrimes(lst.rest);
+        return currElemIsPrime || sq;
     }
 }
